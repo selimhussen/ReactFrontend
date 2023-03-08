b05613af62d61596a19dd1ed62d70a15a42c5529
KalaimaniDiggibyte <126051194+KalaimaniDiggibyte@users.noreply.github.com>
2023-03-08 16:19:42 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8085/').then((response) => {
+      setMessage(response.data)
+    }).catch((error) => {
+      console.log(error);
+    })
+
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message ? message : 'No Data'}</h1>
     </div>
   );
 }
